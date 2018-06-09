12fbf0af4ce83cc255a4f1e49189f67bc9d603b5
Robert Judson <robjudmello@hotmail.com>
2018-06-09 02:48:01 -0300

    Ajustes nas rotas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { TodoComponent } from './todo/todo.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes = [
+  { path: '', component: TodoComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'notfound', component: NotfoundComponent },
+  { path: '**', redirectTo: 'notfound' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    TodoComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
